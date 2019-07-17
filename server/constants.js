cbbd431e04ac50e303cdc6f3ca1635fe4f760411
Abhishek Shrivastava <Abhishek.Shrivastava@rsystems.com>
2019-07-17 15:29:39 +0530

    initial commit

---
+++ server/constants.js
@@ -0,0 +1,10 @@
+/**
+ * Description: define uir at uriConstants
+ */
+
+module.exports = {
+  // GEOSERVICE:
+  //   "http://geo.google.com/geoservice/Out/getGPSBypeeridAndTimestampServlet123"
+  GEOSERVICE:
+       "http://localhost:8083/geoservice/Out/getGPSBypeeridAndTimestampServlet1"
+};
